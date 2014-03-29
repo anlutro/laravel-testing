58cc264050281a3f19fe29acdb2c1912247a3fb8
Andreas Lutro <anlutro@gmail.com>
2014-03-29 07:13:25 +0100

    add comments/docblocks. fix visiblity. move class aliases

--- src/EloquentTestCase.php
+++ src/EloquentTestCase.php
@@ -17,41 +17,73 @@ use Illuminate\Support\Facades\Facade;
 use PHPunit_Framework_TestCase;
 
 /**
+ * Set up class aliases if necessary, as these are quite often used in
+ * migrations, models and seeders.
+ */
+if (!class_exists('Schema')) {
+	class_alias('Illuminate\Support\Facades\Schema', 'Schema');
+}
+if (!class_exists('Eloquent')) {
+	class_alias('Illuminate\Database\Eloquent\Model', 'Eloquent');
+}
+if (!class_exists('Seeder')) {
+	class_alias('Illuminate\Database\Seeder', 'Seeder');
+}
+if (!class_exists('DB')) {
+	class_alias('Illuminate\Support\Facades\DB', 'DB');
+}
+
+/**
  * Abstract test case class for testing models using a test database.
  */
 abstract class EloquentTestCase extends PHPunit_Framework_TestCase
 {
+	/**
+	 * @var \Illuminate\Container\Container
+	 */
 	protected $container;
+
+	/**
+	 * @var \Illuminate\Database\Capsule\Manager
+	 */
 	protected $capsule;
+
+	/**
+	 * @var \Illuminate\Events\Dispatcher
+	 */
 	protected $eventDispatcher;
+
+	/**
+	 * @var \Illuminate\Cache\CacheManager
+	 */
 	protected $cacheManager;
+
+	/**
+	 * Whether to enable events for the test.
+	 *
+	 * @var boolean
+	 */
 	protected $enableEvents = false;
+
+	/**
+	 * Whether to enable cache for the test.
+	 *
+	 * @var boolean
+	 */
 	protected $enableCache = false;
 
 	/**
-	 * Set up class aliases if necessary, as these are quite often used in
-	 * migrations, models and seeders.
+	 * Set up the test case. This method is called before every test method.
+	 *
+	 * If you override this method, remember to call parent::setUp();
 	 */
-	public static function setUpBeforeClass()
-	{
-		if (!class_exists('Schema')) {
-			class_alias('Illuminate\Support\Facades\Schema', 'Schema');
-		}
-		if (!class_exists('Eloquent')) {
-			class_alias('Illuminate\Database\Eloquent\Model', 'Eloquent');
-		}
-		if (!class_exists('Seeder')) {
-			class_alias('Illuminate\Database\Seeder', 'Seeder');
-		}
-		if (!class_exists('DB')) {
-			class_alias('Illuminate\Support\Facades\DB', 'DB');
-		}
-	}
-
 	public function setUp()
 	{
+		// create the IOC container and bind it to the facades.
 		$this->container = new Container;
 		Facade::setFacadeApplication($this->container);
+
+		// create the capsule and bind it to the IOC container.
 		$this->capsule = new DatabaseCapsule($this->container);
 		$this->container['db'] = $this->capsule;
 		$this->capsule->addConnection($this->getDatabaseConfig());
@@ -67,12 +99,20 @@ abstract class EloquentTestCase extends PHPunit_Framework_TestCase
 			$this->capsule->setCacheManager($this->cacheManager);
 		}
 
+		// make the capsule available globally and enable eloquent
 		$this->capsule->setAsGlobal();
 		$this->capsule->bootEloquent();
+
+		// run migrations ans seeds
 		$this->runMigrations('up');
 		$this->runSeeds();
 	}
 
+	/**
+	 * Tear down the test case. This method is called after each test method.
+	 *
+	 * If you override this method, remember to call parent::tearDown();
+	 */
 	public function tearDown()
 	{
 		$this->runMigrations('down');
@@ -84,6 +124,13 @@ abstract class EloquentTestCase extends PHPunit_Framework_TestCase
 		$this->container = null;
 	}
 
+	/**
+	 * Run the test's migrations.
+	 *
+	 * @param  string $direction "up" or "down"
+	 *
+	 * @return void
+	 */
 	protected function runMigrations($direction)
 	{
 		foreach($this->getMigrations() as $class) {
@@ -93,7 +140,12 @@ abstract class EloquentTestCase extends PHPunit_Framework_TestCase
 		}
 	}
 
-	public function runSeeds()
+	/**
+	 * Run the test's seeders.
+	 *
+	 * @return void
+	 */
+	protected function runSeeds()
 	{
 		foreach ($this->getSeeders() as $class) {
 			$seeder = $this->container->make($class);
@@ -103,6 +155,14 @@ abstract class EloquentTestCase extends PHPunit_Framework_TestCase
 		}
 	}
 
+	/**
+	 * Get the database configuration that should be used for the test. This
+	 * corresponds to what would be inside a connection in laravel's default
+	 * app/config/database.php. By default an in-memory SQLite database is used,
+	 * but you can ovverride this to use whatever you like.
+	 *
+	 * @return array
+	 */
 	protected function getDatabaseConfig()
 	{
 		return [
@@ -112,11 +172,23 @@ abstract class EloquentTestCase extends PHPunit_Framework_TestCase
 		];
 	}
 
+	/**
+	 * Get a list of class names for migrations to run for each test. Make sure
+	 * that these classes are autoloaded.
+	 *
+	 * @return string[]
+	 */
 	protected function getMigrations()
 	{
 		return [];
 	}
 
+	/**
+	 * Get a list of class names for seeders to run before each test. Make sure
+	 * that these classes are autoloaded.
+	 *
+	 * @return string[]
+	 */
 	protected function getSeeders()
 	{
 		return [];
